65fbc5e10927fc83ee9bea7bb1de1e1ebb986535
TRIGianni <gianni.tricarico@heh.be>
2021-10-22 09:08:31 +0200

    test persistence adapter with TestContainers

--- src/test/java/be/heh/projetapiweb/PersonPersistenceAdapterTests.java
+++ src/test/java/be/heh/projetapiweb/PersonPersistenceAdapterTests.java
@@ -1,16 +1,26 @@
 package be.heh.projetapiweb;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+@Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({PersonPersistenceAdapter.class})
@@ -20,10 +30,20 @@ public class PersonPersistenceAdapterTests {
     @Autowired
     private PersonPersistenceAdapter personPersistenceAdapter;
 
+   /* @Container
+    public GenericContainer postgres = new GenericContainer(DockerImageName.parse("postgres:13"))
+            .withExposedPorts(5432,5432).withEnv("POSTGRES_PASSWORD","root");*/
+   @ClassRule
+    @Container
+    public static PostgreSQLContainer conteneur = (PostgreSQLContainer) new PostgreSQLContainer(DockerImageName.parse("postgres:13")).
+           withPassword("root").
+           withDatabaseName("postgres").
+           withUsername("postgres");
+
     @Test
     @Sql({"createTable.sql","PersonPersistenceAdapterTests.sql"})
     void getAllPersons(){
-
+        conteneur.start();
         Map<String, Object> map = new HashMap<>();
         ArrayList<Person> pers;
 
